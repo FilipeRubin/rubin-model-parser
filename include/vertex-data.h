f5b5df6c97805e692f4595dc1b84e9e54b333df3
Rubin <filiperubin@gmail.com>
2024-04-30 21:36:32 -0300

    Turn VertexData into a more versatile struct with union members

--- include/vertex-data.h
+++ include/vertex-data.h
@@ -2,10 +2,31 @@
 
 namespace Rubin
 {
-	class VertexData
+	struct VertexData
 	{
-		float  x,  y,  z;
-		float  u,  v;
-		float nx, ny, nz;
+		union
+		{
+			struct
+			{
+				float x, y, z;
+			};
+			float position[3]{ 0.f, 0.f, 0.f };
+		};
+		union
+		{
+			struct
+			{
+				float u, v;
+			};
+			float textureCoordinates[2]{ 0.f, 0.f };
+		};
+		union
+		{
+			struct
+			{
+				float nx, ny, nz;
+			};
+			float normal[3]{ 0.f, 0.f, 0.f };
+		};
 	};
 }
