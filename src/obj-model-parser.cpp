dbde8d450326381e2c2748f35ec3279ec2a95460
Rubin <filiperubin@gmail.com>
2024-05-03 11:50:34 -0300

    Use VerticesBuilder class to improve readability

--- src/obj-model-parser.cpp
+++ src/obj-model-parser.cpp
@@ -1,69 +1,39 @@
 #include "obj-model-parser.h"
-#include "numeric-characters-parser.h"
-#include <array>
+#include "vertices-builder.h"
 #include <sstream>
-#include <vector>
 
+using Rubin::VertexData;
 using Rubin::ModelData;
-using Rubin::NumericCharactersParser;
-
-void FindFloatsInLine(const std::string& line, float* destination, size_t destinationLength)
-{
-	NumericCharactersParser numericCharactersParser;
-
-	unsigned int destinationIndex = 0U;
-
-	for (const char& character : line)
-	{
-		const bool& isLastLineCharacter = character == line.back();
-		const bool& isValidCharacter = NumericCharactersParser::IsValidNumericCharacter(character);
-		const bool& isFloatReadyToParse = (not isValidCharacter or isLastLineCharacter) and not numericCharactersParser.IsEmpty();
-
-		if (isValidCharacter)
-		{
-			numericCharactersParser.AppendCharacter(character);
-		}
-
-		if (isFloatReadyToParse)
-		{
-			destination[destinationIndex] = numericCharactersParser.ParseFloat();
-			numericCharactersParser.ResetBuffer();
-
-			destinationIndex++;
-
-			if (destinationIndex >= destinationLength)
-			{
-				break;
-			}
-		}
-	}
-}
+using Rubin::VerticesBuilder;
 
 ModelData Rubin::OBJModelParser::ParseModel(const std::string source)
 {
-	ModelData result;
-
-    // Find positions
 	std::istringstream sourceStream = std::istringstream(source);
 	std::string line;
 
-	std::unique_ptr<std::vector<VertexData>> vertices = std::make_unique<std::vector<VertexData>>();
+	VerticesBuilder verticesBuilder;
 
 	while (getline(sourceStream, line))
 	{
 		if (line.starts_with("v "))
 		{
-			vertices->push_back(VertexData());
-
-			for (int i = 2; i < line.size(); i++)
-			{
-				const size_t& numberOfElements = sizeof(vertices->back().position) / sizeof(vertices->back().position[0]);
-				FindFloatsInLine(line, vertices->back().position, numberOfElements);
-			}
+			verticesBuilder.FindPositionInLine(line);
+		}
+		else if (line.starts_with("vt"))
+		{
+			verticesBuilder.FindTextureCoordinatesInLine(line);
+		}
+		else if (line.starts_with("vn"))
+		{
+			verticesBuilder.FindNormalInLine(line);
+		}
+		else if (line.starts_with("f"))
+		{
+			verticesBuilder.FindVerticesInLine(line);
 		}
 	}
 
-	result.SetData(std::move(vertices), std::move(std::make_unique<std::vector<unsigned int>>(std::vector<unsigned int>())));
+	ModelData result;
 
 	return result;
 }
