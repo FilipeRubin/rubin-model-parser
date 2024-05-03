ff2bca9fe007706775f869b583a6fe06cbf8c581
Rubin <filiperubin@gmail.com>
2024-05-03 13:17:30 -0300

    Add a struct that stores array indices for building a vertex

---
+++ src/vertex-components-indices.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Rubin
+{
+	struct VertexComponentsIndices
+	{
+		unsigned int positionsIndex;
+		unsigned int textureCoordinatesIndex;
+		unsigned int normalsIndex;
+	};
+}
